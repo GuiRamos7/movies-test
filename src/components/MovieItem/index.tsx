d1aca888bd294613e7c80a2639cf73cb1eec5e0f
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-10-03 23:59:08 -0300

    feat: adding some animation

--- src/components/MovieItem/index.tsx
+++ src/components/MovieItem/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, Image, Text } from '@chakra-ui/react';
 import { AiFillStar } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './styles.css';
 
 interface IMovieIcon {
@@ -22,34 +23,40 @@ const MovieItem = ({
 }: IMovieIcon) => {
   return (
     <Link to={`/movie/${id}`} onClick={onClick}>
-      <Flex direction='column' borderRadius='17px' w='250px' m='auto'>
-        <Image
-          borderRadius='17px'
-          src={`https://image.tmdb.org/t/p/original/${image}`}
-          loading='lazy'
-          alt={`Poster of ${title} movie`}
-          w='250px'
-          h='375'
-        />
-        <Text as='h1' fontSize='xl' fontWeight='700' mt='2' h='45'>
-          {title}
-        </Text>
-        <Text
-          className='description'
-          as='h1'
-          fontSize='xl'
-          py='2'
-          lineHeight='1.7'
-        >
-          {description}
-        </Text>
-        <Flex align='center'>
-          <AiFillStar fill='#ffd200' fontSize='25px' />
-          <Text as='p' fontSize='xl' fontWeight='700' ml='2'>
-            {rate}
+      <motion.div
+        initial={{ opacity: 0, x: '-30px' }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Flex direction='column' borderRadius='17px' w='250px' m='auto'>
+          <Image
+            borderRadius='17px'
+            src={`https://image.tmdb.org/t/p/original/${image}`}
+            loading='lazy'
+            alt={`Poster of ${title} movie`}
+            w='250px'
+            h='375'
+          />
+          <Text as='h1' fontSize='xl' fontWeight='700' mt='2' h='45'>
+            {title}
           </Text>
+          <Text
+            className='description'
+            as='h1'
+            fontSize='xl'
+            py='2'
+            lineHeight='1.7'
+          >
+            {description}
+          </Text>
+          <Flex align='center'>
+            <AiFillStar fill='#ffd200' fontSize='25px' />
+            <Text as='p' fontSize='xl' fontWeight='700' ml='2'>
+              {rate}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
+      </motion.div>
     </Link>
   );
 };
--- src/pages/MoviePage/index.tsx
+++ src/pages/MoviePage/index.tsx
@@ -4,6 +4,7 @@ import { AiFillStar } from 'react-icons/ai';
 import { HiBookmark, HiOutlineX, HiPlay } from 'react-icons/hi';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import { Button } from 'components';
 import type { RootState } from 'config/store';
@@ -55,65 +56,89 @@ const MoviePage = () => {
         px={['0', '0', '6']}
         direction='column'
       >
-        <Text fontSize={['3xl', '4xl', '5xl']} color='white' fontWeight='bold'>
-          {data?.movie.title}
-        </Text>
-        <Flex
-          align={['start', 'start', 'center']}
-          flexDirection={['column', 'column', 'row']}
+        <motion.div
+          initial={{ opacity: 0, x: '-50px' }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          <Flex align='center'>
-            <AiFillStar fill='#ffd200' fontSize='25px' />
-            <Text as='p' fontSize='xl' ml='2'>
-              {data?.movie.vote_average} | {data?.movie.vote_count}
-            </Text>
+          <Text
+            fontSize={['3xl', '4xl', '5xl']}
+            color='white'
+            fontWeight='bold'
+          >
+            {data?.movie.title}
+          </Text>
+
+          <Flex
+            align={['start', 'start', 'center']}
+            flexDirection={['column', 'column', 'row']}
+          >
+            <Flex align='center'>
+              <AiFillStar fill='#ffd200' fontSize='25px' />
+              <Text as='p' fontSize='xl' ml='2'>
+                {data?.movie.vote_average} | {data?.movie.vote_count}
+              </Text>
+            </Flex>
+            <Flex ml={['0', '0', '10']}>
+              <Text as='p' fontSize={['md', 'lg', 'xl']} ml='2'>
+                {data?.movie.runtime}min &#8226; {genres} &#8226;{' '}
+                {convertDate(data?.movie.release_date ?? '')}
+              </Text>
+            </Flex>
           </Flex>
-          <Flex ml={['0', '0', '10']}>
-            <Text as='p' fontSize={['md', 'lg', 'xl']} ml='2'>
-              {data?.movie.runtime}min &#8226; {genres} &#8226;{' '}
-              {convertDate(data?.movie.release_date ?? '')}
-            </Text>
-          </Flex>
-        </Flex>
-        <Flex
-          w={['100%', '100%', '70%']}
-          mt='10'
-          fontSize={['lg', 'xl', '2xl']}
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: '-80px' }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          {data?.movie.overview}
-        </Flex>
-        <Flex mt='10' flexDirection={['column', 'column', 'row']}>
-          <Button
-            mr='5'
-            leftIcon={<HiPlay />}
-            bg='blue.500'
-            fontSize='xl'
-            mt='5'
-            fontWeight='normal'
+          <Flex
+            w={['100%', '100%', '70%']}
+            mt='10'
+            fontSize={['lg', 'xl', '2xl']}
           >
-            Play now
-          </Button>
-          <Button
-            mt='5'
-            fontWeight='normal'
-            fontSize='xl'
-            leftIcon={!isFavorite() ? <HiBookmark /> : <HiOutlineX />}
-            bg={!isFavorite() ? 'gray.100' : 'orange.500'}
-            onClick={() =>
-              !isFavorite()
-                ? dispatch(
-                    addFavorites({
-                      title: data?.movie.title ?? '',
-                      image: data?.movie.poster_path ?? '',
-                      id: movieID ?? 0,
-                    })
-                  )
-                : dispatch(removeFavorites(movieID ?? 0))
-            }
-          >
-            {!isFavorite() ? 'Save on your list' : 'Remove from your list'}
-          </Button>
-        </Flex>
+            {data?.movie.overview}
+          </Flex>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: '-120px' }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Flex mt='10' flexDirection={['column', 'column', 'row']}>
+            <Button
+              mr='5'
+              leftIcon={<HiPlay />}
+              bg='blue.500'
+              fontSize='xl'
+              mt='5'
+              fontWeight='normal'
+            >
+              Play now
+            </Button>
+            <Button
+              mt='5'
+              fontWeight='normal'
+              fontSize='xl'
+              leftIcon={!isFavorite() ? <HiBookmark /> : <HiOutlineX />}
+              bg={!isFavorite() ? 'gray.100' : 'orange.500'}
+              onClick={() =>
+                !isFavorite()
+                  ? dispatch(
+                      addFavorites({
+                        title: data?.movie.title ?? '',
+                        image: data?.movie.poster_path ?? '',
+                        id: movieID ?? 0,
+                      })
+                    )
+                  : dispatch(removeFavorites(movieID ?? 0))
+              }
+            >
+              {!isFavorite() ? 'Save on your list' : 'Remove from your list'}
+            </Button>
+          </Flex>
+        </motion.div>
       </Flex>
     </Flex>
   );
